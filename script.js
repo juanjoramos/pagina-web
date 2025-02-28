a4e29643f01bdb6583591d1ca9b413e7ae792404
juanjoramos <juan.ramos586@pascualbravo.edu.co>
2025-02-28 01:48:31 -0500

    actualizacion pagina web

---
+++ script.js
@@ -0,0 +1,58 @@
+// Mostrar mensaje al hacer clic
+function mostrarMensaje() {
+    const mensaje = document.getElementById('mensaje');
+    mensaje.textContent = "¡Hola! Has hecho clic en el botón. 🚀";
+}
+
+// Validación del formulario
+(function() {
+    'use strict';
+
+    const form = document.getElementById('contactoForm');
+    const alerta = document.getElementById('alertaExito');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (form.checkValidity()) {
+            alerta.classList.remove('d-none');
+            form.reset();
+            form.classList.remove('was-validated');
+        } else {
+            alerta.classList.add('d-none');
+            form.classList.add('was-validated');
+        }
+    }, false);
+
+})();
+
+/*SLIDER*/
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+//mueve el slider en la direccion especificada (1 para avanzar, -1 para retroceder)
+function moveSlide(direction) {
+    currentIndex += direction;
+
+    //asegura que el indice este dentro de los limites
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;//vuelve a la primara imagen
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;//vuelve a la ultima
+    }
+    updateSliderPosition();
+}
+
+// actualiza la posición del slider segun el indice actual
+function updateSliderPosition() {
+    const slider = document.querySelector('.slider');
+    const offset = -currentIndex * 100;//calcula el desplazamiento en porcentaje
+    slider.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(() => {
+    moveSlide(1);
+}, 10000);
+
